8ca1f1510edb8a42ea9a203ffd3abcf50d3771dd
Koz Ross <koz.ross@retro-freedom.nz>
2025-02-21 12:31:40 +1300

    BROKEN: partial work

--- src/Covenant/ASGNode.hs
+++ src/Covenant/ASGNode.hs
@@ -1,21 +1,149 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Covenant.ASGNode
-  ( Id (..),
+  ( TypeError (..),
+    Id (..),
     typeId,
     Arg (..),
     Bound (..),
     Ref (..),
     typeRef,
     CompNode,
+    ValNode,
+    ASGNode,
+    typeASGNode,
   )
 where
 
+import Control.Monad ((>=>))
 import Control.Monad.Except (MonadError (throwError))
 import Control.Monad.HashCons (MonadHashCons (lookupRef))
+import Control.Monad.Reader (MonadReader (local))
 import Covenant.Constant (AConstant)
 import Covenant.Ledger (LedgerAccessor, LedgerDestructor)
-import Covenant.Prim (OneArgFunc, SixArgFunc, ThreeArgFunc, TwoArgFunc)
-import Covenant.Type (CompT, ValT)
+import Covenant.Prim
+  ( OneArgFunc
+      ( BData,
+        BLS12_381_G1_compress,
+        BLS12_381_G1_neg,
+        BLS12_381_G1_uncompress,
+        BLS12_381_G2_compress,
+        BLS12_381_G2_neg,
+        BLS12_381_G2_uncompress,
+        Blake2b_244,
+        Blake2b_256,
+        ComplementByteString,
+        CountSetBits,
+        DecodeUtf8,
+        EncodeUtf8,
+        FindFirstSetBit,
+        FstPair,
+        HeadList,
+        IData,
+        Keccak_256,
+        LengthOfByteString,
+        ListData,
+        MapData,
+        MkNilData,
+        MkNilPairData,
+        NullList,
+        Ripemd_160,
+        SerialiseData,
+        Sha2_256,
+        Sha3_256,
+        SndPair,
+        TailList,
+        UnBData,
+        UnConstrData,
+        UnIData,
+        UnListData,
+        UnMapData
+      ),
+    SixArgFunc (CaseData, ChooseData),
+    ThreeArgFunc
+      ( AndByteString,
+        CaseList,
+        ChooseList,
+        ExpModInteger,
+        IfThenElse,
+        IntegerToByteString,
+        OrByteString,
+        VerifyEcdsaSecp256k1Signature,
+        VerifyEd25519Signature,
+        VerifySchnorrSecp256k1Signature,
+        WriteBits,
+        XorByteString
+      ),
+    TwoArgFunc
+      ( AddInteger,
+        AppendByteString,
+        AppendString,
+        BLS12_381_G1_add,
+        BLS12_381_G1_equal,
+        BLS12_381_G1_hashToGroup,
+        BLS12_381_G1_scalarMul,
+        BLS12_381_G2_add,
+        BLS12_381_G2_equal,
+        BLS12_381_G2_hashToGroup,
+        BLS12_381_G2_scalarMul,
+        BLS12_381_finalVerify,
+        BLS12_381_millerLoop,
+        BLS12_381_mulMlResult,
+        ByteStringToInteger,
+        ChooseUnit,
+        ConsByteString,
+        ConstrData,
+        DivideInteger,
+        EqualsByteString,
+        EqualsData,
+        EqualsInteger,
+        EqualsString,
+        IndexByteString,
+        LessThanByteString,
+        LessThanEqualsByteString,
+        LessThanEqualsInteger,
+        LessThanInteger,
+        MkCons,
+        MkPairData,
+        ModInteger,
+        MultiplyInteger,
+        QuotientInteger,
+        ReadBit,
+        RemainderInteger,
+        ReplicateByte,
+        RotateByteString,
+        ShiftByteString,
+        SubtractInteger,
+        Trace
+      ),
+  )
+import Covenant.Type
+  ( AbsTy (AbsTy),
+    BuiltinT
+      ( BLS12_381_G1_ElementT,
+        BLS12_381_G2_ElementT,
+        BLS12_381_MlResultT,
+        BoolT,
+        ByteStringT,
+        DataT,
+        IntegerT,
+        ListT,
+        PairT,
+        StringT,
+        UnitT
+      ),
+    CompT (ForallT, FunT, ReturnT),
+    ValT (AbsT, BValT, ThunkT),
+    pattern (:--:>),
+    pattern (:-->),
+    pattern (::--::>),
+    pattern (::--:>),
+    pattern (::-->),
+  )
 import Data.Kind (Type)
+import Data.Vector (Vector)
+import Data.Vector qualified as Vector
+import Data.Vector.NonEmpty (NonEmptyVector)
 import Data.Word (Word64)
 
 -- | @since 1.0.0
@@ -25,23 +153,16 @@ data TypeError
     -- being improperly reused outside of their enclosing 'MonadHashCons'
     -- computation.
     BrokenIdReference Id
-  | -- | Attempt to return the specified computation type, instead of a value
-    -- type.
+  | -- Attempt to forall a value type.
+    ForallValType ValT
+  | -- Attempt to return a computation type.
     ReturnCompType CompT
-  | -- | Attempt to force the specified computation type, instead of a thunk.
+  | -- Attempt to force a computation type.
     ForceCompType CompT
-  | -- | Attempt to force a non-thunk value type.
-    ForceNonThunk ValT
-  | -- | Attempt to apply an argument to a value type.
-    ApplyToValueType ValT
-  | -- | Attempt to use a computation type as an argument.
-    CompTypeArgument CompT
-  | -- | Lambda returns a value type.
-    LambdaReturnValueType ValT
-  | -- | Attempt to apply arguments to a return.
-    ReturnApplyArgument ValT
-  | -- Incompatible argument types in application: expected argument, then actual argument.
-    IncompatibleArgumentTypes ValT ValT
+  | -- Attempt to bind an expression of computation type.
+    LetBindingCompType CompT
+  | -- Attempt to use a value type as a @let@ body.
+    LetBodyValType ValT
   deriving stock (Eq, Show)
 
 -- | A unique identifier for a node in a Covenant program.
@@ -73,9 +194,7 @@ typeId ::
 typeId i =
   lookupRef i >>= \case
     Nothing -> throwError . BrokenIdReference $ i
-    Just asgNode -> pure $ case asgNode of
-      ACompNode t _ -> Left t
-      AValNode t _ -> Right t
+    Just asgNode -> pure . typeASGNode $ asgNode
 
 data Arg = Arg ValT Word64
   deriving stock (Eq, Ord, Show)
@@ -109,39 +228,237 @@ typeRef = \case
   AnId i -> typeId i
   ABound (Bound t _) -> pure . pure $ t
 
-data CompNode
-  = LamInternal ValT Id
-  | ForceInternal Ref
-  | ReturnInternal Ref
-  | AppInternal Id Ref
+data Primitive
+  = Prim1 OneArgFunc
+  | Prim2 TwoArgFunc
+  | Prim3 ThreeArgFunc
+  | Prim6 SixArgFunc
   deriving stock (Eq, Ord, Show)
 
--- | A call to a Plutus primitive.
---
--- @since 1.0.0
-data PrimCall
-  = PrimCallOne OneArgFunc Ref
-  | PrimCallTwo TwoArgFunc Ref Ref
-  | PrimCallThree ThreeArgFunc Ref Ref Ref
-  | PrimCallSix SixArgFunc Ref Ref Ref Ref Ref Ref
-  deriving stock
-    ( -- | @since 1.0.0
-      Eq,
-      -- | @since 1.0.0
-      Ord,
-      -- | @since 1.0.0
-      Show
-    )
+typePrimitive :: Primitive -> CompT
+typePrimitive = \case
+  Prim1 p -> typeOneArg p
+  Prim2 p -> typeTwoArg p
+  Prim3 p -> typeThreeArg p
+  Prim6 p -> typeSixArg p
+
+data CompNode
+  = TyAbsInternal Id
+  | LamInternal (Vector ValT) Id
+  | BuiltinInternal Primitive
+  | LetInternal ValT Ref Id
+  | ForceInternal Ref
+  | ReturnInternal Ref
+  deriving stock (Eq, Ord, Show)
+
+newtype TyInsts = TyInsts (Vector (Maybe ValT))
+
+freshInst :: TyInsts -> TyInsts
+freshInst (TyInsts v) = TyInsts . Vector.cons Nothing $ v
+
+typeCompNode ::
+  forall (m :: Type -> Type).
+  (MonadHashCons Id ASGNode m, MonadError TypeError m, MonadReader TyInsts m) =>
+  CompNode -> m CompT
+typeCompNode = \case
+  TyAbsInternal i ->
+    local freshInst $
+      typeId i >>= \case
+        Left compT -> pure . ForallT $ compT
+        Right t -> throwError . ForallValType $ t
+  LamInternal argTs i -> _
+  BuiltinInternal p -> pure . typePrimitive $ p
+  LetInternal bindingT binding body ->
+    typeRef binding >>= \case
+      Left t -> throwError . LetBindingCompType $ t
+      Right valT ->
+        typeId body >>= \case
+          Left compT -> unify bindingT valT compT
+          Right t -> throwError . LetBodyValType $ t
+  ForceInternal r ->
+    typeRef r >>= \case
+      Left t -> throwError . ForceCompType $ t
+      Right (ThunkT compT) -> pure compT
+      Right t -> throwError . ForceNonThunk $ t
+  ReturnInternal r ->
+    typeRef r >>= \case
+      Left t -> throwError . ReturnCompType $ t
+      Right valT -> pure . ReturnT $ valT
 
 data ValNode
   = LitInternal AConstant
   | ThunkInternal Id
-  | ReceiveInternal Id
-  | LetInternal Ref Ref
-  | PrimAppInternal PrimCall
+  | AppInternal Id (Vector Ref)
   | LedgerAccessInternal LedgerAccessor Ref
   | LedgerDestructInternal LedgerDestructor Id Ref
   deriving stock (Eq, Ord, Show)
 
 data ASGNode = ACompNode CompT CompNode | AValNode ValT ValNode
   deriving stock (Eq, Ord, Show)
+
+typeASGNode :: ASGNode -> Either CompT ValT
+typeASGNode = \case
+  ACompNode t _ -> Left t
+  AValNode t _ -> Right t
+
+-- Helpers
+
+typeOneArg :: OneArgFunc -> CompT
+typeOneArg = \case
+  LengthOfByteString -> ByteStringT ::--::> IntegerT
+  Sha2_256 -> ByteStringT ::--::> ByteStringT
+  Sha3_256 -> ByteStringT ::--::> ByteStringT
+  Blake2b_256 -> ByteStringT ::--::> ByteStringT
+  EncodeUtf8 -> StringT ::--::> ByteStringT
+  DecodeUtf8 -> ByteStringT ::--::> StringT
+  FstPair ->
+    ForallT . ForallT $ PairT absOuter absInner ::--:> absOuter
+  SndPair ->
+    ForallT . ForallT $ PairT absOuter absInner ::--:> absInner
+  HeadList ->
+    ForallT $ ListT absInner ::--:> absInner
+  TailList ->
+    ForallT $ ListT absInner ::--::> ListT absInner
+  NullList ->
+    ForallT $ ListT absInner ::--::> BoolT
+  MapData ->
+    ListT (BValT . PairT (BValT DataT) . BValT $ DataT) ::--::> DataT
+  ListData ->
+    ListT (BValT DataT) ::--::> DataT
+  IData -> IntegerT ::--::> DataT
+  BData -> ByteStringT ::--::> DataT
+  UnConstrData ->
+    DataT ::--::> PairT (BValT IntegerT) (BValT (ListT (BValT DataT)))
+  UnMapData ->
+    DataT ::--::> ListT (BValT (PairT (BValT DataT) . BValT $ DataT))
+  UnListData ->
+    DataT ::--::> ListT (BValT DataT)
+  UnIData -> DataT ::--::> IntegerT
+  UnBData -> DataT ::--::> ByteStringT
+  SerialiseData -> DataT ::--::> ByteStringT
+  -- Note (Koz, 20/02/2025): Technically both of these are functions in Plutus
+  -- Core, which take a unit argument. This was needed as a workaround for a
+  -- lack of nullary functions, but since we can be a bit more honest, this is
+  -- really just a way of making these lazy constants. Thus, we use `ReturnT` as
+  -- their type. It means they can't be applied to anything, but then again,
+  -- it's not like it makes a tonne of sense to do so anyway.
+  MkNilData -> ReturnT . BValT . ListT . BValT $ DataT
+  MkNilPairData ->
+    ReturnT . BValT . ListT . BValT . PairT (BValT DataT) . BValT $ DataT
+  BLS12_381_G1_neg -> BLS12_381_G1_ElementT ::--::> BLS12_381_G1_ElementT
+  BLS12_381_G1_compress -> BLS12_381_G1_ElementT ::--::> ByteStringT
+  BLS12_381_G1_uncompress -> ByteStringT ::--::> BLS12_381_G1_ElementT
+  BLS12_381_G2_neg -> BLS12_381_G2_ElementT ::--::> BLS12_381_G2_ElementT
+  BLS12_381_G2_compress -> BLS12_381_G2_ElementT ::--::> ByteStringT
+  BLS12_381_G2_uncompress -> ByteStringT ::--::> BLS12_381_G2_ElementT
+  Keccak_256 -> ByteStringT ::--::> ByteStringT
+  Blake2b_244 -> ByteStringT ::--::> ByteStringT
+  ComplementByteString -> ByteStringT ::--::> ByteStringT
+  CountSetBits -> ByteStringT ::--::> IntegerT
+  FindFirstSetBit -> ByteStringT ::--::> IntegerT
+  Ripemd_160 -> ByteStringT ::--::> ByteStringT
+
+typeTwoArg :: TwoArgFunc -> CompT
+typeTwoArg = \case
+  AddInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  SubtractInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  MultiplyInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  DivideInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  QuotientInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  RemainderInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  ModInteger -> IntegerT ::--> IntegerT ::--::> IntegerT
+  EqualsInteger -> IntegerT ::--> IntegerT ::--::> BoolT
+  LessThanInteger -> IntegerT ::--> IntegerT ::--::> BoolT
+  LessThanEqualsInteger -> IntegerT ::--> IntegerT ::--::> BoolT
+  AppendByteString -> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  ConsByteString -> IntegerT ::--> ByteStringT ::--::> ByteStringT
+  IndexByteString -> ByteStringT ::--> IntegerT ::--::> ByteStringT
+  EqualsByteString -> ByteStringT ::--> ByteStringT ::--::> BoolT
+  LessThanByteString -> ByteStringT ::--> ByteStringT ::--::> BoolT
+  LessThanEqualsByteString -> ByteStringT ::--> ByteStringT ::--::> BoolT
+  AppendString -> StringT ::--> StringT ::--::> StringT
+  EqualsString -> StringT ::--> StringT ::--::> BoolT
+  ChooseUnit -> ForallT (UnitT ::--> absInner :--:> absInner)
+  Trace -> ForallT (StringT ::--> absInner :--:> absInner)
+  MkCons -> ForallT (absInner :--> ListT absInner ::--::> ListT absInner)
+  ConstrData -> IntegerT ::--> ListT (BValT DataT) ::--::> DataT
+  EqualsData -> DataT ::--> DataT ::--::> BoolT
+  MkPairData -> DataT ::--> DataT ::--::> PairT (BValT DataT) (BValT DataT)
+  BLS12_381_G1_add ->
+    BLS12_381_G1_ElementT ::--> BLS12_381_G1_ElementT ::--::> BLS12_381_G1_ElementT
+  BLS12_381_G1_scalarMul ->
+    IntegerT ::--> BLS12_381_G1_ElementT ::--::> BLS12_381_G1_ElementT
+  BLS12_381_G1_equal ->
+    BLS12_381_G1_ElementT ::--> BLS12_381_G1_ElementT ::--::> BoolT
+  BLS12_381_G1_hashToGroup ->
+    ByteStringT ::--> ByteStringT ::--::> BLS12_381_G1_ElementT
+  BLS12_381_G2_add ->
+    BLS12_381_G2_ElementT ::--> BLS12_381_G2_ElementT ::--::> BLS12_381_G2_ElementT
+  BLS12_381_G2_scalarMul ->
+    IntegerT ::--> BLS12_381_G2_ElementT ::--::> BLS12_381_G2_ElementT
+  BLS12_381_G2_equal ->
+    BLS12_381_G2_ElementT ::--> BLS12_381_G2_ElementT ::--::> BoolT
+  BLS12_381_G2_hashToGroup ->
+    ByteStringT ::--> ByteStringT ::--::> BLS12_381_G2_ElementT
+  BLS12_381_millerLoop ->
+    BLS12_381_G1_ElementT ::--> BLS12_381_G2_ElementT ::--::> BLS12_381_MlResultT
+  BLS12_381_mulMlResult ->
+    BLS12_381_MlResultT ::--> BLS12_381_MlResultT ::--::> BLS12_381_MlResultT
+  BLS12_381_finalVerify ->
+    BLS12_381_MlResultT ::--> BLS12_381_MlResultT ::--::> BoolT
+  ByteStringToInteger -> BoolT ::--> ByteStringT ::--::> IntegerT
+  ReadBit -> ByteStringT ::--> IntegerT ::--::> BoolT
+  ReplicateByte -> IntegerT ::--> IntegerT ::--::> ByteStringT
+  ShiftByteString -> ByteStringT ::--> IntegerT ::--::> ByteStringT
+  RotateByteString -> ByteStringT ::--> IntegerT ::--::> ByteStringT
+
+typeThreeArg :: ThreeArgFunc -> CompT
+typeThreeArg = \case
+  VerifyEd25519Signature ->
+    ByteStringT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  VerifyEcdsaSecp256k1Signature ->
+    ByteStringT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  VerifySchnorrSecp256k1Signature ->
+    ByteStringT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  -- Note (Koz, 20/02/2025): This is strict in both branches. Only use this if you
+  -- are absolutely, totally sure you don't mind evaluating both branches.
+  IfThenElse ->
+    ForallT (BoolT ::--> absInner :--> absInner :--:> absInner)
+  ChooseList ->
+    ForallT . ForallT $ ListT absOuter ::--> absInner :--> absInner :--:> absInner
+  CaseList ->
+    ForallT . ForallT $ absInner :--> ThunkT (absOuter :--> ListT absOuter ::--:> absInner) :--> ListT absOuter ::--:> absInner
+  IntegerToByteString -> BoolT ::--> IntegerT ::--> IntegerT ::--::> ByteStringT
+  AndByteString -> BoolT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  OrByteString -> BoolT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  XorByteString -> BoolT ::--> ByteStringT ::--> ByteStringT ::--::> ByteStringT
+  WriteBits ->
+    ByteStringT ::--> ListT (BValT IntegerT) ::--> BoolT ::--::> ByteStringT
+  ExpModInteger -> IntegerT ::--> IntegerT ::--> IntegerT ::--::> IntegerT
+
+typeSixArg :: SixArgFunc -> CompT
+typeSixArg = \case
+  ChooseData ->
+    ForallT $
+      DataT
+        ::--> absInner
+        :--> absInner
+        :--> absInner
+        :--> absInner
+        :--> absInner
+        :--:> absInner
+  CaseData ->
+    ForallT $
+      ThunkT (IntegerT ::--> ListT (BValT DataT) ::--:> absInner)
+        :--> ThunkT (ListT (BValT (PairT (BValT DataT) (BValT DataT))) ::--:> absInner)
+        :--> ThunkT (ListT (BValT DataT) ::--:> absInner)
+        :--> ThunkT (IntegerT ::--:> absInner)
+        :--> ThunkT (ByteStringT ::--:> absInner)
+        :--> DataT
+        ::--:> absInner
+
+absOuter :: ValT
+absOuter = AbsT . AbsTy $ 1
+
+absInner :: ValT
+absInner = AbsT . AbsTy $ 0
--- src/Covenant/Type.hs
+++ src/Covenant/Type.hs
@@ -1,27 +1,33 @@
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 module Covenant.Type
   ( AbsTy (..),
     LedgerT (..),
     BuiltinT (..),
     ValT (..),
+    valTAbstractions,
     CompT (..),
+    compTAbstractions,
+    pattern (:-->),
+    pattern (:--:>),
+    pattern (::-->),
+    pattern (::--:>),
+    pattern (::--::>),
   )
 where
 
+import Control.Monad (guard)
+import Data.EnumMap.Strict (EnumMap)
+import Data.EnumMap.Strict qualified as EnumMap
+import Data.EnumSet (EnumSet)
+import Data.EnumSet qualified as EnumSet
+import Data.Functor (($>))
 import Data.Word (Word64)
 
--- | @since 1.0.0
 newtype AbsTy = AbsTy Word64
-  deriving
-    ( -- | @since 1.0.0
-      Eq,
-      -- | @since 1.0.0
-      Ord
-    )
-    via Word64
-  deriving stock
-    ( -- | @since 1.0.0
-      Show
-    )
+  deriving (Eq, Enum, Ord) via Word64
+  deriving stock (Show)
 
 -- Note (Koz, 17/02/2025): This only covers V3.Contexts for now.
 data LedgerT
@@ -47,6 +53,9 @@ data LedgerT
   | ScriptContextT
   deriving stock (Eq, Ord, Show)
 
+ledgerTAbstractions :: LedgerT -> EnumSet AbsTy
+ledgerTAbstractions = const EnumSet.empty
+
 data BuiltinT
   = UnitT
   | BoolT
@@ -62,13 +71,114 @@ data BuiltinT
   | PairT ValT ValT
   deriving stock (Eq, Ord, Show)
 
+builtinTAbstractions :: BuiltinT -> EnumSet AbsTy
+builtinTAbstractions = \case
+  LedgerType t -> ledgerTAbstractions t
+  ListT t -> valTAbstractions t
+  PairT t1 t2 -> valTAbstractions t1 <> valTAbstractions t2
+  _ -> EnumSet.empty
+
 data ValT
   = BValT BuiltinT
-  | AbsT AbsTy
   | ThunkT CompT
+  | AbsT AbsTy
   deriving stock (Eq, Ord, Show)
+
+valTAbstractions :: ValT -> EnumSet AbsTy
+valTAbstractions = \case
+  BValT bi -> builtinTAbstractions bi
+  ThunkT t -> compTAbstractions t
+  AbsT t -> EnumSet.singleton t
+
+unifyValT :: ValT -> ValT -> Maybe (EnumMap AbsTy ValT)
+unifyValT t1 t2 = do
+  let t1Abs = valTAbstractions t1
+  let t2Abs = valTAbstractions t2
+  if
+    -- The types are both concrete, so equality will do
+    | EnumSet.null t1Abs && EnumSet.null t2Abs ->
+        guard (t1 == t2) $> EnumMap.empty
+    -- Only one type has any abstractions, so we use this to 'drive'
+    -- unification
+    | EnumSet.null t1Abs -> go EnumMap.empty t2 t1
+    | EnumSet.null t2Abs -> go EnumMap.empty t1 t2
+    -- Both types have abstractions
+    | otherwise -> _
+  where
+    go :: EnumMap AbsTy ValT -> ValT -> ValT -> Maybe (EnumMap AbsTy ValT)
+    go acc hasAbs noAbs = case hasAbs of
+      AbsT abs -> pure $ acc <> EnumMap.singleton abs noAbs
+      BValT bi -> case noAbs of
+        BValT bi' -> goInner acc bi bi'
+        _ -> Nothing
+      ThunkT compT -> case noAbs of
+        ThunkT compT' -> goThunk acc compT compT'
+        _ -> Nothing
+    goInner :: EnumMap AbsTy ValT -> BuiltinT -> BuiltinT -> Maybe (EnumMap AbsTy ValT)
+    goInner acc hasAbs noAbs = case hasAbs of
+      LedgerType t -> case noAbs of
+        LedgerType t' -> guard (t == t') $> acc
+        _ -> Nothing
+      ListT t -> case noAbs of
+        ListT t' -> go acc t t'
+        _ -> Nothing
+      PairT t1 t2 -> case noAbs of
+        PairT t1' t2' -> go acc t1 t1' >>= \acc' -> go acc' t2 t2'
+        _ -> Nothing
+      _ -> guard (hasAbs == noAbs) $> acc
+    goThunk :: EnumMap AbsTy ValT -> CompT -> CompT -> Maybe (EnumMap AbsTy ValT)
+    goThunk acc hasAbs noAbs = case hasAbs of
+      FunT argT resultT -> case noAbs of
+        FunT argT' resultT' -> go acc argT argT' >>= \acc' -> goThunk acc' resultT resultT'
+        _ -> Nothing
+      ForallT t -> case noAbs of
+        ForallT t' -> goThunk acc t t'
+        _ -> Nothing
+      ReturnT t -> case noAbs of
+        ReturnT t' -> go acc t t'
+        _ -> Nothing
 
 data CompT
   = FunT ValT CompT
+  | ForallT CompT
   | ReturnT ValT
   deriving stock (Eq, Ord, Show)
+
+compTAbstractions :: CompT -> EnumSet AbsTy
+compTAbstractions = \case
+  FunT argT resultT -> valTAbstractions argT <> compTAbstractions resultT
+  ForallT compT -> compTAbstractions compT
+  ReturnT valT -> valTAbstractions valT
+
+pattern (:-->) :: ValT -> CompT -> CompT
+pattern x :--> y <- FunT x y
+  where
+    x :--> y = FunT x y
+
+infixr 1 :-->
+
+pattern (::-->) :: BuiltinT -> CompT -> CompT
+pattern x ::--> y <- FunT (BValT x) y
+  where
+    x ::--> y = FunT (BValT x) y
+
+infixr 1 ::-->
+
+pattern (:--:>) :: ValT -> ValT -> CompT
+pattern x :--:> y <- FunT x (ReturnT y)
+  where
+    x :--:> y = FunT x (ReturnT y)
+
+pattern (::--:>) :: BuiltinT -> ValT -> CompT
+pattern x ::--:> y <- FunT (BValT x) (ReturnT y)
+  where
+    x ::--:> y = FunT (BValT x) (ReturnT y)
+
+infixr 1 ::--:>
+
+pattern (::--::>) :: BuiltinT -> BuiltinT -> CompT
+pattern x ::--::> y <- FunT (BValT x) (ReturnT (BValT y))
+  where
+    x ::--::> y = FunT (BValT x) (ReturnT (BValT y))
+
+infixr 1 ::--::>
