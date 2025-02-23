2f80b9d5a264f8b983c2becbb14c473bd35e4813
Koz Ross <koz.ross@retro-freedom.nz>
2025-02-24 09:38:12 +1300

    Redraft

--- src/Control/Monad/Unify.hs
+++ src/Control/Monad/Unify.hs
@@ -13,37 +13,28 @@
 -- * [MicroKanren
 -- paper](http://webyrd.net/scheme-2013/papers/HemannMuKanren2013.pdf)
 module Control.Monad.Unify
-  ( UnifyResult (..),
+  ( UnifyStatus (..),
     UnifyT,
     runUnifyT,
     MonadUnify (..),
   )
 where
 
-import Control.Monad.Reader (ReaderT, asks, local, runReaderT)
+import Control.Monad (unless)
+import Control.Monad.State.Strict (StateT, gets, modify, runStateT)
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Maybe (MaybeT, hoistMaybe, runMaybeT)
+import Data.EnumMap.Strict (EnumMap)
+import Data.EnumMap.Strict qualified as EnumMap
 import Data.Kind (Type)
-import Data.List.NonEmpty (NonEmpty ((:|)))
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as Map
 import Data.Set.NonEmpty (NESet)
 import Data.Set.NonEmpty qualified as NESet
-import Test.QuickCheck
-  ( CoArbitrary (coarbitrary),
-    Function (function),
-    functionMap,
-    variant,
-  )
 
--- | The outcome of a unification attempt.
---
--- @since 1.0.0
-data UnifyResult (var :: Type) (def :: Type)
-  = -- | Unable to unify the given arguments
-    DoesNotUnify var (Either var def)
-  | -- | The arguments belong to the same equivalence class
-    Equivalent
-  | -- | The argument unify to a definition, which is given
-    UnifiesTo def
+-- | @since 1.0.0
+data UnifyStatus (var :: Type) (def :: Type)
+  = Fresh
+  | Equiv (NESet var)
+  | Defined def
   deriving stock
     ( -- | @since 1.0.0
       Eq,
@@ -52,35 +43,8 @@ data UnifyResult (var :: Type) (def :: Type)
     )
 
 -- | @since 1.0.0
-instance (CoArbitrary var, CoArbitrary def) => CoArbitrary (UnifyResult var def) where
-  {-# INLINEABLE coarbitrary #-}
-  coarbitrary res = case res of
-    DoesNotUnify x y -> variant (0 :: Int) . coarbitrary x . coarbitrary y
-    Equivalent -> variant (1 :: Int)
-    UnifiesTo x -> variant (2 :: Int) . coarbitrary x
-
--- | @since 1.0.0
-instance (Function var, Function def) => Function (UnifyResult var def) where
-  {-# INLINEABLE function #-}
-  function = functionMap into outOf
-    where
-      into :: UnifyResult var def -> Either (var, Either var def) (Maybe def)
-      into = \case
-        DoesNotUnify x y -> Left (x, y)
-        Equivalent -> Right Nothing
-        UnifiesTo x -> Right (Just x)
-      outOf :: Either (var, Either var def) (Maybe def) -> UnifyResult var def
-      outOf = \case
-        Left (x, y) -> DoesNotUnify x y
-        Right Nothing -> Equivalent
-        Right (Just x) -> UnifiesTo x
-
--- | An implementation of MicroKanren-like unification as a concrete monadic
--- stack.
---
--- @since 1.0.0
 newtype UnifyT (var :: Type) (def :: Type) (m :: Type -> Type) (a :: Type)
-  = UnifyT (ReaderT (UnifyEnv var def) m a)
+  = UnifyT (StateT (EnumMap var (Either (NESet var) def)) (MaybeT m) a)
   deriving
     ( -- | @since 1.0.0
       Functor,
@@ -89,156 +53,90 @@ newtype UnifyT (var :: Type) (def :: Type) (m :: Type -> Type) (a :: Type)
       -- | @since 1.0.0
       Monad
     )
-    via ReaderT (UnifyEnv var def) m
+    via StateT (EnumMap var (Either (NESet var) def)) (MaybeT m)
 
 -- | Execute all unifications.
 --
 -- @since 1.0.0
 runUnifyT ::
   forall (var :: Type) (def :: Type) (m :: Type -> Type) (a :: Type).
+  (Monad m) =>
   UnifyT var def m a ->
-  m a
-runUnifyT (UnifyT comp) = runReaderT comp . UnifyEnv $ Map.empty
+  m (Maybe (a, EnumMap var (Either (NESet var) def)))
+runUnifyT (UnifyT comp) = runMaybeT . runStateT comp $ EnumMap.empty
 
--- | A capability type class specifying that a stack can perform
--- MicroKanren-style unification.
+-- | = Laws
 --
--- = Laws
+-- In the description below, @f@ is as follows:
 --
--- 1. @'unify' x y (\_ -> 'unify' x y f)@ @=@
---    @'unify' x y f@
--- 2. @'unify' v1 ('Left' v2) (\res1 -> unify v2 ('Left' v1) (f res1))@
---    @=@
---    @'unify' v2 ('Left' v1) (\res2 -> unify v1 ('Left' v2) (\res1 -> f res1 res2)@
+-- @
+-- f v = case v of
+--    'Fresh' -> 'pure' ()
+--    'Equiv' vs -> 'unify' v ('Left' ('NESet.findMin' vs))
+--    'Defined' x -> 'unify' v ('Right' x)
+-- @
+--
+-- 1. @'status' v '>>' 'status' v@ @=@ @'status' v@
+-- 2. @'unify' v x '>>' 'unify' v x@ @=@ @'unify' v x@
+-- 3. @'unify' v1 x '>>' 'unify' v2 y@ @=@
+--    @'unify' v2 y '>>' 'unify' v1 x@
+-- 3. @'unify' v1 ('Left' v2)@ @=@ @'unify' v2 ('Left' v1)@
+-- 4. @'status' v '>>=' f@ @=@ @'pure' ()@
 --
 -- @since 1.0.0
 class (Monad m) => MonadUnify var def m | m -> var def where
-  -- | Given a variable, and either a variable or definition, assert that the
-  -- these two arguments belong to the same equivalence class. Then, call the
-  -- third argument with the outcome of that assertion; the computation inside
-  -- the callback argument will note the outcome of this assertion if either, or
-  -- both, arguments were fresh. If unification fails, the environment does not
-  -- change.
-  --
-  -- @since 1.0.0
-  unify ::
-    forall (a :: Type).
-    var ->
-    Either var def ->
-    (UnifyResult var def -> m a) ->
-    m a
+  -- | @since 1.0.0
+  status :: var -> m (UnifyStatus var def)
+
+  -- | @since 1.0.0
+  unify :: var -> Either var def -> m ()
 
 -- | @since 1.0.0
-instance (Eq def, Ord var, Monad m) => MonadUnify var def (UnifyT var def m) where
+instance (Monad m, Enum var, Eq def, Ord var) => MonadUnify var def (UnifyT var def m) where
+  {-# INLINEABLE status #-}
+  status v =
+    UnifyT $ do
+      lookedUp <- gets (EnumMap.lookup v)
+      pure $ case lookedUp of
+        Nothing -> Fresh
+        Just (Left vars) -> Equiv vars
+        Just (Right x) -> Defined x
   {-# INLINEABLE unify #-}
-  unify ::
-    forall (a :: Type).
-    var ->
-    Either var def ->
-    (UnifyResult var def -> UnifyT var def m a) ->
-    UnifyT var def m a
-  unify var1 y f = case y of
-    Left var2 -> do
-      defAbs1 <- UnifyT . asks $ definedAs var1
-      defAbs2 <- UnifyT . asks $ definedAs var2
-      case defAbs1 of
-        Nothing -> UnifyT $ case defAbs2 of
-          Nothing -> local (addNewEC var1 var2) (callback Equivalent)
-          Just (Left def2) -> local (define var1 def2) (callback . UnifiesTo $ def2)
-          Just (Right ec2) -> local (growEC var1 ec2) (callback Equivalent)
-        Just (Left def1) -> case defAbs2 of
-          Nothing -> UnifyT $ local (define var2 def1) (callback . UnifiesTo $ def1)
-          Just (Left def2) -> unifyConcrete def1 def2
-          Just (Right ec2) -> UnifyT $ local (defineEC ec2 def1) (callback . UnifiesTo $ def1)
-        Just (Right ec1) -> UnifyT $ case defAbs2 of
-          Nothing -> local (growEC var2 ec1) (callback Equivalent)
-          Just (Left def2) -> local (defineEC ec1 def2) (callback . UnifiesTo $ def2)
-          Just (Right ec2) -> local (unifyECs ec1 ec2) (callback Equivalent)
-    Right def -> handleMixed var1 def
+  unify v x = UnifyT $ do
+    lookedUp <- gets (EnumMap.lookup v)
+    case lookedUp of
+      Nothing -> case x of
+        Left v' -> do
+          lookedUp' <- gets (EnumMap.lookup v')
+          modify $ case lookedUp' of
+            Nothing ->
+              EnumMap.insert v (Left . NESet.singleton $ v') . EnumMap.insert v' (Left . NESet.singleton $ v)
+            Just (Left vars) -> \acc ->
+              let newEC = NESet.insert v vars
+               in NESet.foldl' (go newEC) acc newEC
+            Just (Right def) -> EnumMap.insert v (Right def)
+        Right x' -> modify (EnumMap.insert v (Right x'))
+      Just (Left vars) -> case x of
+        Left v' ->
+          let newEC = NESet.insert v' vars
+           in modify (\acc -> NESet.foldl' (go newEC) acc newEC)
+        Right x' -> modify (\acc -> NESet.foldl' (go2 x') acc vars)
+      Just (Right def) -> case x of
+        Left v' -> do
+          lookedUp' <- gets (EnumMap.lookup v')
+          case lookedUp' of
+            Nothing -> modify (EnumMap.insert v' (Right def))
+            Just (Left vars) ->
+              let newEC = NESet.insert v' vars
+               in modify (\acc -> NESet.foldl' (go newEC) acc newEC)
+            Just (Right def') -> unless (def == def') (lift . hoistMaybe $ Nothing)
+        Right x' -> unless (def == x') (lift . hoistMaybe $ Nothing)
     where
-      handleMixed :: var -> def -> UnifyT var def m a
-      handleMixed var def =
-        (UnifyT . asks $ definedAs var) >>= \case
-          Nothing -> UnifyT $ local (define var def) (callback (UnifiesTo def))
-          Just (Left def') -> unifyConcrete def def'
-          Just (Right ec) -> UnifyT $ local (defineEC ec def) (callback (UnifiesTo def))
-      unifyConcrete :: def -> def -> UnifyT var def m a
-      unifyConcrete def1 def2 = f (if def1 == def2 then UnifiesTo def1 else DoesNotUnify var1 y)
-      callback :: UnifyResult var def -> ReaderT (UnifyEnv var def) m a
-      callback arg = let UnifyT comp = f arg in comp
-
--- Helpers
-
--- Internal unification state, which connects a variable to either a concrete
--- definition, or to an equivalence class of other variables whose definition
--- doesn't (yet) exist.
-newtype UnifyEnv (var :: Type) (def :: Type)
-  = UnifyEnv (Map var (Either def (NESet var)))
-  deriving (Eq) via (Map var (Either def (NESet var)))
-  deriving stock (Show)
-
--- Helper for querying the state of a variable. It can either be fresh
--- (`Nothing`), bound to a definition (`Just . Left`) or part of an equivalence
--- class without a definition (`Just . Right`).
-definedAs ::
-  forall (abs :: Type) (conc :: Type).
-  (Ord abs) =>
-  abs -> UnifyEnv abs conc -> Maybe (Either conc (NESet abs))
-definedAs x (UnifyEnv xs) = Map.lookup x xs
-
--- Given two fresh variables, construct a new equivalence class consisting of
--- just them, and augment the environment with them.
-addNewEC ::
-  forall (var :: Type) (def :: Type).
-  (Ord var) =>
-  var -> var -> UnifyEnv var def -> UnifyEnv var def
-addNewEC x y (UnifyEnv xs) =
-  let newEC = NESet.fromList $ x :| [y]
-   in UnifyEnv . Map.insert x (Right newEC) . Map.insert y (Right newEC) $ xs
-
--- Given a fresh variable and an already-existing equivalence class, expand that
--- equivalence class with the variable in the environment.
-growEC ::
-  forall (var :: Type) (def :: Type).
-  (Ord var) =>
-  var -> NESet var -> UnifyEnv var def -> UnifyEnv var def
-growEC extra ec (UnifyEnv xs) = UnifyEnv . NESet.foldl' go xs $ expanded
-  where
-    expanded :: NESet var
-    expanded = NESet.insert extra ec
-    go :: Map var (Either def (NESet var)) -> var -> Map var (Either def (NESet var))
-    go acc x = Map.insert x (Right expanded) acc
-
--- Given a variable (which may or may not be fresh) and a definition, place that
--- variable into the equivalence class of that definition in the environment.
-define ::
-  forall (var :: Type) (def :: Type).
-  (Ord var) =>
-  var -> def -> UnifyEnv var def -> UnifyEnv var def
-define x def (UnifyEnv xs) = UnifyEnv . Map.insert x (Left def) $ xs
-
--- Given two already-existing equivalence classes, combine them into one
--- equivalence class in the environment.
-unifyECs ::
-  forall (var :: Type) (def :: Type).
-  (Ord var) =>
-  NESet var -> NESet var -> UnifyEnv var def -> UnifyEnv var def
-unifyECs ec1 ec2 (UnifyEnv xs) = UnifyEnv . NESet.foldl' go xs $ unified
-  where
-    unified :: NESet var
-    unified = NESet.union ec1 ec2
-    go :: Map var (Either def (NESet var)) -> var -> Map var (Either def (NESet var))
-    go acc x = Map.insert x (Right unified) acc
-
--- Given an already-existing equivalence class (without a definition), and a
--- desired definition without an equivalence class in the environment, define
--- that equivalence class to now be that of the given definition in the
--- environment.
-defineEC ::
-  forall (var :: Type) (def :: Type).
-  (Ord var) =>
-  NESet var -> def -> UnifyEnv var def -> UnifyEnv var def
-defineEC ec def (UnifyEnv xs) = UnifyEnv . NESet.foldl' go xs $ ec
-  where
-    go :: Map var (Either def (NESet var)) -> var -> Map var (Either def (NESet var))
-    go acc x = Map.insert x (Left def) acc
+      go ::
+        NESet var ->
+        EnumMap var (Either (NESet var) def) ->
+        var ->
+        EnumMap var (Either (NESet var) def)
+      go newEC acc v' = EnumMap.insert v' (Left newEC) acc
+      go2 :: def -> EnumMap var (Either (NESet var) def) -> var -> EnumMap var (Either (NESet var) def)
+      go2 x' acc v' = EnumMap.insert v' (Right x') acc
--- test/monad-unify/Main.hs
+++ test/monad-unify/Main.hs
@@ -1,42 +1,29 @@
 module Main (main) where
 
 import Control.Monad.Unify
-  ( UnifyResult (DoesNotUnify, Equivalent, UnifiesTo),
+  ( UnifyStatus (Defined, Fresh),
     UnifyT,
     runUnifyT,
+    status,
     unify,
   )
 import Data.Functor.Identity (Identity, runIdentity)
 import Data.Kind (Type)
 import Test.QuickCheck
-  ( Fun,
+  ( Arbitrary (arbitrary, shrink),
     Property,
-    applyFun,
-    applyFun2,
-    arbitrary,
     forAllShrinkShow,
-    shrink,
     (===),
   )
-import Test.QuickCheck.Monadic (assert, monadic, pre, run)
-import Test.QuickCheck.Poly (B, C, OrdA)
+import Test.QuickCheck.Poly (B)
 import Test.Tasty (adjustOption, defaultMain, testGroup)
-import Test.Tasty.QuickCheck (QuickCheckMaxSize, QuickCheckTests, testProperty)
+import Test.Tasty.QuickCheck (QuickCheckTests, testProperty)
 
 main :: IO ()
 main =
   defaultMain . adjustOption moreTests . testGroup "MonadUnify" $
-    [ testProperty "equal constants unify" equalConstantProp,
-      adjustOption bigger $ testProperty "unequal constants do not unify" differentConstantProp,
-      testProperty "fresh variables are equivalent" freshVarEquivProp,
-      testProperty "binding any element of an equivalence class binds all" bindECProp,
-      testProperty "variable-to-variable binding is reflexive" ecReflexiveProp,
-      testProperty "variable-to-variable binding is symmetric" ecSymmetricProp,
-      testProperty "variable-to-variable binding is transitive" ecTransitiveProp,
-      testProperty "bound variable unifies with binding" boundBindingUnifyProp,
-      adjustOption bigger $ testProperty "bound variables does not unify with anything else" boundNotBindingProp,
-      testProperty "MonadUnify law 1" law1Prop,
-      testProperty "MonadUnify law 2" law2Prop
+    [ testProperty "Unify fresh with definition" propFreshDef,
+      testProperty "Fresh variables' status agrees" propFreshStatus
     ]
   where
     -- Note (Koz, 18/02/2025): By default, QuickCheck runs very few tests for
@@ -44,92 +31,24 @@ main =
     -- tests, while not blocking us from asking for more via the CLI.
     moreTests :: QuickCheckTests -> QuickCheckTests
     moreTests = max 10_000
-    -- Note (Koz, 18/02/2025): When using `pre`, the default max size of 100 is
-    -- very likely to cause collisions, leading to an unacceptably high discard
-    -- rate. This ensures we minimize collisions, while also allowing larger
-    -- sizes to be set at the CLI if we want them.
-    bigger :: QuickCheckMaxSize -> QuickCheckMaxSize
-    bigger = max 10_000
 
 -- Properties
 
-equalConstantProp :: Property
-equalConstantProp = forAllShrinkShow arbitrary shrink show $ \(var1 :: OrdA, var2 :: OrdA, c :: B) ->
-  monadic runTestM $ do
-    result <- run . unify var1 (Right c) $ (\_ -> unify var2 (Right c) pure)
-    assert (result == UnifiesTo c)
+propFreshDef :: Property
+propFreshDef = forAllShrinkShow arbitrary shrink show $ \(v :: EnumA, def :: B) ->
+  let result = runTestM (unify v (Right def) >> status v)
+   in result === Just (Defined def)
 
-differentConstantProp :: Property
-differentConstantProp = forAllShrinkShow arbitrary shrink show $ \(var :: OrdA, c1 :: B, c2 :: B) ->
-  monadic runTestM $ do
-    pre (c1 /= c2)
-    result <- run . unify var (Right c1) $ (\_ -> unify var (Right c2) pure)
-    assert (result == DoesNotUnify var (Right c2))
-
-freshVarEquivProp :: Property
-freshVarEquivProp = forAllShrinkShow arbitrary shrink show $ \(var1 :: OrdA, var2 :: OrdA) ->
-  monadic runTestM $ do
-    result <- run . unify var1 (Left var2 :: Either OrdA B) $ pure
-    assert (result == Equivalent)
-
-bindECProp :: Property
-bindECProp = forAllShrinkShow arbitrary shrink show $ \(var1 :: OrdA, var2 :: OrdA, c :: B) ->
-  monadic runTestM $ do
-    bindOuter <- run . unify var1 (Left var2) $ (\_ -> unify var1 (Right c) (\_ -> unify var2 (Right c) pure))
-    bindInner <- run . unify var1 (Left var2) $ (\_ -> unify var2 (Right c) (\_ -> unify var1 (Right c) pure))
-    assert (bindOuter == bindInner)
-
-ecReflexiveProp :: Property
-ecReflexiveProp = forAllShrinkShow arbitrary shrink show $ \(var :: OrdA) ->
-  monadic runTestM $ do
-    result <- run . unify var (Left var :: Either OrdA B) $ pure
-    assert (result == Equivalent)
-
-ecSymmetricProp :: Property
-ecSymmetricProp = forAllShrinkShow arbitrary shrink show $ \(var1 :: OrdA, var2 :: OrdA) ->
-  monadic runTestM $ do
-    bind1Then2 <- run . unify var1 (Left var2 :: Either OrdA B) $ (\_ -> unify var2 (Left var1) pure)
-    bind2Then1 <- run . unify var2 (Left var1) $ (\_ -> unify var1 (Left var2) pure)
-    assert (bind1Then2 == bind2Then1)
-
-ecTransitiveProp :: Property
-ecTransitiveProp = forAllShrinkShow arbitrary shrink show $ \(var1 :: OrdA, var2 :: OrdA, var3 :: OrdA) ->
-  monadic runTestM $ do
-    result <- run . unify var1 (Left var2 :: Either OrdA B) $ (\_ -> unify var2 (Left var3) $ \_ -> unify var1 (Left var3) pure)
-    assert (result == Equivalent)
-
-boundBindingUnifyProp :: Property
-boundBindingUnifyProp = forAllShrinkShow arbitrary shrink show $ \(var :: OrdA, c :: B) ->
-  monadic runTestM $ do
-    result <- run . unify var (Right c) $ pure
-    assert (result == UnifiesTo c)
-
-boundNotBindingProp :: Property
-boundNotBindingProp = forAllShrinkShow arbitrary shrink show $ \(var :: OrdA, c1 :: B, c2 :: B) ->
-  monadic runTestM $ do
-    pre (c1 /= c2)
-    result <- run . unify var (Right c1) $ (\_ -> unify var (Right c2) pure)
-    assert (result == DoesNotUnify var (Right c2))
-
--- Note (Koz, 18/02/2025): These are not perfect law tests, as we have to ignore
--- auxilliary effects (because our inner monad is `Identity`), and we don't test
--- calls to `unify` in the laws nested within other definitions. However, these
--- are so hard to figure out how to test as such, much less sensibly, that this
--- is the best we can do for now.
-
-law1Prop :: Property
-law1Prop = forAllShrinkShow arbitrary shrink show $ \(x :: OrdA, y :: Either OrdA B, f :: Fun (UnifyResult OrdA B) C) ->
-  let lhs = runTestM (unify x y (\_ -> unify x y (pure . applyFun f)))
-      rhs = runTestM (unify x y (pure . applyFun f))
-   in lhs === rhs
-
-law2Prop :: Property
-law2Prop = forAllShrinkShow arbitrary shrink show $ \(v1 :: OrdA, v2 :: OrdA, f :: Fun (UnifyResult OrdA B, UnifyResult OrdA B) C) ->
-  let lhs = runTestM (unify v1 (Left v2) (\res1 -> unify v2 (Left v1) (pure . applyFun2 f res1)))
-      rhs = runTestM (unify v2 (Left v1) (\res2 -> unify v1 (Left v2) (\res1 -> pure . applyFun2 f res1 $ res2)))
-   in lhs === rhs
+propFreshStatus :: Property
+propFreshStatus = forAllShrinkShow arbitrary shrink show $ \(v :: EnumA) ->
+  let result = runTestM (status v)
+   in result === Just Fresh
 
 -- Helpers
 
-runTestM :: forall (a :: Type). UnifyT OrdA B Identity a -> a
-runTestM = runIdentity . runUnifyT
+newtype EnumA = EnumA Word
+  deriving (Eq, Ord, Enum, Arbitrary) via Word
+  deriving stock (Show)
+
+runTestM :: forall (a :: Type). UnifyT EnumA B Identity a -> Maybe a
+runTestM = fmap fst . runIdentity . runUnifyT
