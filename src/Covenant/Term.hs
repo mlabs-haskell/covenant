3d662399da0a6d3ea94d29e6b44fd31d812c0012
Koz Ross <koz.ross@retro-freedom.nz>
2025-04-08 12:36:25 +1200

    Initial

---
+++ src/Covenant/Term.hs
@@ -0,0 +1,26 @@
+module Covenant.Term
+  ( Id (..),
+  )
+where
+
+-- | A unique identifier for a node in a Covenant program.
+--
+-- @since 1.0.0
+newtype Id = Id Word64
+  deriving
+    ( -- | @since 1.0.0
+      Eq,
+      -- | @since 1.0.0
+      Ord,
+      -- | @since 1.0.0
+      Bounded,
+      -- | Needed for internal reasons, even though this type class is terrible.
+      --
+      -- @since 1.0.0
+      Enum
+    )
+    via Word64
+  deriving stock
+    ( -- | @since 1.0.0
+      Show
+    )
